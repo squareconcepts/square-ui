f79f973cca3359229267ae8125495d44d085494f
Olrik Nijmeijer <olrik@squareconcepts.nl>
2023-10-11 15:29:59 +0200

    Reverted component

--- src/LivewireComponents/LocalizedStringComponent.php
+++ src/LivewireComponents/LocalizedStringComponent.php
@@ -1,28 +1,39 @@
 <?php
 namespace Squareconcepts\SquareUi\LivewireComponents;
 
-use Livewire\Attributes\Modelable;
-use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class LocalizedStringComponent extends Component
 {
-    #[Modelable] #[Rule('nullable')]
-    public ?array $value = [];
+    public string $field;
+    public array $value;
+    public string $event;
+    public string $identifier = '';
+    public string $rerenderValueEvent = 'unknown';
+
+    protected function getListeners()
+    {
+        return [
+            $this->rerenderValueEvent => 'rerenderValue'
+        ];
+    }
 
     protected $rules = [
         'value.*' => 'nullable|array'
     ];
 
-    public function mount()
-    {
-        if($this->value === null) {
-            $this->value = [];
-        }
-    }
-
     public function render()
     {
         return view('square-ui::livewire-components.localized-string-component');
+    }
+
+    public function emitValue(): void
+    {
+        $this->dispatch($this->event, $this->field, $this->value, $this->identifier);
+    }
+
+    public function rerenderValue($data): void
+    {
+        $this->value = $data;
     }
 }
