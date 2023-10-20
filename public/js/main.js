bca0199b6a0ab21a367fbbe8bd0c9953672e6ddc
Olrik Nijmeijer <olrik@squareconcepts.nl>
2023-10-20 14:57:08 +0200

    Fixed bugs in slide comp

--- public/js/main.js
+++ public/js/main.js
@@ -64,18 +64,27 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('slideOverPanel', (id) => ({
         id: id,
         showing: false,
-        toggle() {
-            document.getElementById('slideover-'+ this.id +'-container').classList.toggle('invisible');
-            document.getElementById('slideover-'+ this.id +'-bg').classList.toggle('opacity-0');
-            document.getElementById('slideover-'+ this.id +'-bg').classList.toggle('opacity-50');
-            document.getElementById('slideover-'+ this.id).classList.toggle('translate-x-full');
+        open() {
+            document.getElementById('slideover-'+ this.id +'-container').classList.add('invisible');
+            document.getElementById('slideover-'+ this.id +'-bg').classList.add('opacity-0');
+            document.getElementById('slideover-'+ this.id +'-bg').classList.add('opacity-50');
+            document.getElementById('slideover-'+ this.id).classList.add('translate-x-full');
+            setTimeout(() => {
+                this.showing = !this.showing;
+            }, 10)
+        },
+        close() {
+            document.getElementById('slideover-'+ this.id +'-container').classList.remove('invisible');
+            document.getElementById('slideover-'+ this.id +'-bg').classList.remove('opacity-0');
+            document.getElementById('slideover-'+ this.id +'-bg').classList.remove('opacity-50');
+            document.getElementById('slideover-'+ this.id).classList.remove('translate-x-full');
             setTimeout(() => {
                 this.showing = !this.showing;
             }, 10)
         },
         clickOutside(show) {
             if(this.showing) {
-                this.toggle();
+                this.close();
             }
         }
     }));
