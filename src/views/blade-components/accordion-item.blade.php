8f726bbddb9dc1d99a559d17822fa14f908304c6
Arthur Doorgeest <arthur.doorgeest@outlook.com>
2023-10-24 10:32:38 +0200

    Update accordion-item.blade.php

--- src/views/blade-components/accordion-item.blade.php
+++ src/views/blade-components/accordion-item.blade.php
@@ -1,12 +1,17 @@
 <li class="relative border-b border-gray-200" >
-    <button type="button" class="w-full px-8 py-6 text-left bg-slate-100" @click="selected !== {{ $index }} ? selected = {{ $index }} : selected = null">
-        <div class="flex items-center justify-between">
+    <div type="button" class="w-full px-8 py-6 text-left bg-slate-100" @click="selected !== {{ $index }} ? selected = {{ $index }} : selected = null">
+        <div class="flex items-center gap-2">
+           @isset($buttons)
+                <div class="flex items-center gap-2">
+                    {!! $buttons !!}
+                </div>
+           @endif
             <span>
                 {{ $title }}
             </span>
-            <i class="fa" :class="selected == {{ $index }} ? 'fa-chevron-down' : 'fa-chevron-up'"></i>
+            <i class="ml-auto fa" :class="selected == {{ $index }} ? 'fa-chevron-down' : 'fa-chevron-up'"></i>
         </div>
-    </button>
+    </div>
     <div class="relative overflow-hidden transition-all duration-700 max-h-0" style="" x-ref="container{{ $index }}"
          x-bind:style="selected == {{ $index }} ? 'max-height: ' + ($refs.container{{ $index }}.scrollHeight == 0 ? '1000px' : $refs.container{{ $index }}.scrollHeight + 'px') : ''"
     >
