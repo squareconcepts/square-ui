d34526c3df864b6800cb84f22f6a7ae41865bdfb
Arthur Doorgeest <arthur.doorgeest@outlook.com>
2023-10-24 13:29:14 +0200

    Update accordion-item.blade.php

--- src/views/blade-components/accordion-item.blade.php
+++ src/views/blade-components/accordion-item.blade.php
@@ -1,12 +1,12 @@
 <li class="relative border-b border-gray-200" >
-    <div class="w-full px-8 py-6 text-left bg-slate-100 hover:bg-slate-200" >
+    <div class="w-full px-8 text-left bg-slate-100 hover:bg-slate-200" >
         <div class="flex items-center gap-2 cursor-pointer">
            @isset($buttons)
                 <div class="flex items-center gap-2">
                     {!! $buttons !!}
                 </div>
            @endif
-            <div class="flex-1 flex items-center gap-2 cursor-pointer" @click="selected !== {{ $index }} ? selected = {{ $index }} : selected = null">
+            <div class="flex-1 flex items-center gap-2 cursor-pointer  py-6" @click="selected !== {{ $index }} ? selected = {{ $index }} : selected = null">
                 <span >
                     {{ $title }}
                 </span>
