2dec68484cc7f66473df067a13a72e9161766fa3
Arthur Doorgeest <arthur.doorgeest@outlook.com>
2023-11-29 12:07:31 +0100

    accordion fix

--- src/views/blade-components/accordion-item.blade.php
+++ src/views/blade-components/accordion-item.blade.php
@@ -1,11 +1,11 @@
 <li class="relative border-b border-gray-200"  {{$attributes}}>
     <div class="w-full px-8 text-left bg-slate-100 hover:bg-slate-200" >
         <div class="flex items-center gap-2 cursor-pointer">
-           @isset($buttons)
-                <div class="flex items-center gap-2">
+            @isset($buttons)
+                <div class="flex items-center gap-1">
                     {!! $buttons !!}
                 </div>
-           @endif
+            @endif
             <div class="flex-1 flex items-center gap-2 cursor-pointer  py-6" @click="selected !== {{ $index }} ? selected = {{ $index }} : selected = null">
                 <span >
                     {{ $title }}
@@ -15,8 +15,15 @@
 
         </div>
     </div>
-    <div class="relative overflow-hidden transition-all duration-700 max-h-0" style="" x-ref="container{{ $index }}"
-         x-bind:style="selected == {{ $index }} ? 'max-height: 1000px': ''"
+    <div style="" x-ref="container{{ $index }}" x-cloak
+         x-show="selected == {{ $index }}"
+         x-transition:enter="transition ease-out duration-300"
+         x-transition:enter-start="opacity-0 scale-90"
+         x-transition:enter-end="opacity-100 scale-100"
+         x-transition:leave="transition ease-in duration-300"
+         x-transition:leave-start="opacity-100 scale-100"
+         x-transition:leave-end="opacity-0 scale-90"
+        {{--         x-bind:style="selected == {{ $index }} ? 'max-height: 10000px': ''"--}}
     >
         <div class="p-6">
             {{ $slot }}
