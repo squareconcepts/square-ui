d649eb573d345edc26fef5d2b65d34141632d681
Jordi Benckhuijsen <jordi@squareconcepts.nl>
2023-12-20 13:38:27 +0100

    fixed country flags

--- src/views/livewire-components/localized-string-component.blade.php
+++ src/views/livewire-components/localized-string-component.blade.php
@@ -4,7 +4,7 @@
             <x-input wire:model="value.{{ config('app.locale') }}" class="pl-12 py-2" wire:keyup="emitValue">
                 <x-slot name="prepend">
                     <div class="absolute inset-y-0 left-0 flex items-center p-0.5 z-20 cursor-pointer" x-on:click="showDropdown = true">
-                        <x-flag-country-{{ config('square-ui.languages')[config('app.locale')] }} class="w-6 h-6 ml-2"/>
+                        <x-dynamic-component component="flag-country-{{ config('square-ui.languages')[config('app.locale')] }}" class="w-6 h-6 ml-2"/>
                     </div>
                 </x-slot>
             </x-input>
@@ -15,7 +15,7 @@
                     <x-input wire:model="value.{{ $lang }}" class="pl-12 py-2" wire:keyup="emitValue">
                         <x-slot name="prepend">
                             <div class="absolute inset-y-0 left-0 flex items-center p-0.5 z-20">
-                                <x-flag-country-{{ $flag }} class="w-6 h-6 ml-2 "/>
+                                <x-dynamic-component component="flag-country-{{ $flag }}" class="w-6 h-6 ml-2 "/>
                             </div>
                         </x-slot>
                     </x-input>
