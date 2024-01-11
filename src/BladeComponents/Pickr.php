4c8eaaff5cf654c3796f5a8f27ae7895cd497ce1
Arthur Doorgeest <arthur.doorgeest@outlook.com>
2024-01-11 10:57:01 +0100

    label added

--- src/BladeComponents/Pickr.php
+++ src/BladeComponents/Pickr.php
@@ -6,7 +6,13 @@
 
     class Pickr extends Component {
 
-        public function __construct(public string $value, public string $componentId, public string $identifier, public array $colorOptions = ['#cf3c25', '#d66e20', '#52b268', '#52b268', '#51b47c', '#2c6f2f', '#465c5f', '#2c3a3d', '#1d2626', '#1eb9d7', '#1c9cc4'])
+        public function __construct(
+            public string $value,
+            public string $componentId,
+            public string $identifier,
+            public array $colorOptions = ['#cf3c25', '#d66e20', '#52b268', '#52b268', '#51b47c', '#2c6f2f', '#465c5f', '#2c3a3d', '#1d2626', '#1eb9d7', '#1c9cc4'],
+            public string $label = ''
+        )
         {
         }
 
--- src/views/blade-components/pickr.blade.php
+++ src/views/blade-components/pickr.blade.php
@@ -1,17 +1,21 @@
 @props(['value' => '#000000'])
-<!-- One of the following themes -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/classic.min.css"/> <!-- 'classic' theme -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/monolith.min.css"/> <!-- 'monolith' theme -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/nano.min.css"/> <!-- 'nano' theme -->
+<div wire:ignore>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/classic.min.css"/> <!-- 'classic' theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/monolith.min.css"/> <!-- 'monolith' theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/nano.min.css"/> <!-- 'nano' theme -->
+    <script src="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/pickr.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/pickr.es5.min.js"></script>
+    <div class="flex justify-between items-end mb-1">
+        <label class="block text-sm font-medium text-gray-700 dark:text-gray-400">
+            {{$label}}
+        </label>
+    </div>
+    <div
 
-<!-- Modern or es5 bundle -->
-<script src="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/pickr.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/pickr.es5.min.js"></script>
-<div
-    wire:ignore
-    x-data="pickr('{{ $identifier }}', '{{ $value}}', '{{ $attributes['wire:model'] }}', '{{ $componentId }}')" x-init="initPickr"
-    x-ref="pickr_{{$identifier}}"
->
+        x-data="pickr('{{ $identifier }}', '{{ $value}}', '{{ $attributes['wire:model'] }}', '{{ $componentId }}')" x-init="initPickr"
+        x-ref="pickr_{{$identifier}}"
+    >
+    </div>
     <script>
         document.addEventListener('alpine:init', () => {
             initPickr()
@@ -27,7 +31,7 @@
                 model: model,
                 componentId: componentId,
                 initPickr() {
-                   let  pickr = Pickr.create({
+                    let  pickr = Pickr.create({
                         el: this.$refs['pickr_' + this.identifier],
                         default: this.value,
                         comparison: false,
@@ -46,16 +50,16 @@
                                 save: true
                             }
                         },
-                       i18n: {
-                           'ui:dialog': 'color picker dialog',
-                           'btn:toggle': 'toggle color picker dialog',
-                           'btn:swatch': 'color swatch',
-                           'btn:last-color': 'use previous color',
-                           'btn:save': 'Opslaan',
-                           'btn:cancel': 'Annuleren',
-                           'btn:clear': 'Wissen',
+                        i18n: {
+                            'ui:dialog': 'color picker dialog',
+                            'btn:toggle': 'toggle color picker dialog',
+                            'btn:swatch': 'color swatch',
+                            'btn:last-color': 'use previous color',
+                            'btn:save': 'Opslaan',
+                            'btn:cancel': 'Annuleren',
+                            'btn:clear': 'Wissen',
 
-                       }});
+                        }});
 
                     pickr.on('change', (color, instance) => {
                         this.value =  color.toHEXA().toString();
