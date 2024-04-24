425941eb224f2ddf50b6a2dc66b32747fc9bb4f1
Arthur Doorgeest <arthur.doorgeest@outlook.com>
2024-04-24 09:54:51 +0200

    zet de tijd op een uur die ook een optie is

--- src/BladeComponents/Inputs/Date.php
+++ src/BladeComponents/Inputs/Date.php
@@ -9,7 +9,10 @@
     class Date extends Component
     {
 
-        public function __construct(public string $model, public null|string|Carbon $value = null, public bool $withTime = false, public array $hourOptions = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23], public array $minuteOptions = [0,5,10,15,20,25,30,35,40,45,50,55])
+        /**
+         * @throws \Exception
+         */
+        public function __construct( public string $model, public null|string|Carbon $value = null, public bool $withTime = false, public array $hourOptions = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23], public array $minuteOptions = [0,5,10,15,20,25,30,35,40,45,50,55])
         {
             if($this->value == null) {
                 $this->value = now();
@@ -19,20 +22,66 @@
                 }
             }
 
-            if($this->value instanceof Carbon) {
+            if($this->withTime && count($this->hourOptions) < 1) {
+                throw new \Exception('Hour options must contain at least one value to use with time.');
+            }
+
+            if($this->value instanceof Carbon && $this->withTime) {
                 $minutes = $this->value->minute;
+                $hour = $this->value->hour;
+                $this->value->setHour($this->getFirstAvailableHourOption($hour));
+                if(count($this->minuteOptions) == 1){
+                    $rounding = $this->minuteOptions[0];
 
-                // Bereken hoeveel minuten naar het volgende veelvoud van 5 minuten moeten worden afgerond
-                $remainder = $minutes % $this->minuteOptions[1];
-                $rounding = $this->minuteOptions[1] - $remainder;
+                    if($rounding > $minutes) {
+                        $this->value->setMinutes($rounding);
+                    } else {
+                        $this->value = $this->value->setMinutes($rounding);
 
-                // Als de huidige minuten al het dichtstbijzijnde veelvoud van 5 minuten zijn, retourneer het datetime-object ongewijzigd
-                if ( $remainder === 0 ) {
-                    return;
+                        $this->value = $this->value->addHour();
+
+                        $this->value->setHour($this->getFirstAvailableHourOption($this->value->hour));
+                    }
+                } elseif (count($this->minuteOptions) > 1) {
+                    // Bereken hoeveel minuten naar het volgende veelvoud van 5 minuten moeten worden afgerond
+                    $remainder = $minutes % $this->minuteOptions[1];
+                    $rounding = $this->minuteOptions[1] - $remainder;
+
+                    // Als de huidige minuten al het dichtstbijzijnde veelvoud van 5 minuten zijn, retourneer het datetime-object ongewijzigd
+                    if ( $remainder === 0 ) {
+                        return;
+                    }
+                    // Anders rond naar het dichtstbijzijnde veelvoud van 5 minuten
+                    $this->value = $this->value->addMinutes($rounding);
+                } else {
+                    $this->minuteOptions = range(0,59);
                 }
 
-                // Anders rond naar het dichtstbijzijnde veelvoud van 5 minuten
-                $this->value = $this->value->addMinutes($rounding);
+            }
+        }
+
+        private function getFirstAvailableHourOption( int $hour )
+        {
+            if(in_array($hour, $this->hourOptions) ) {
+                return $hour;
+            } else {
+                $max = max($this->hourOptions);
+                $min = min($this->hourOptions);
+
+                if($hour < $min ) {
+                    return $min;
+                } else if ( $hour > $max ) {
+                    return $min;
+                } else {
+                    $closestGreater = $max;
+                    foreach ($this->hourOptions as $option) {
+                        if ($option > $hour) {
+                            $closestGreater = $option;
+                            break; // Stop de lus zodra de eerste waarde groter is dan $hour
+                        }
+                    }
+                    return $closestGreater;
+                }
             }
         }
 
